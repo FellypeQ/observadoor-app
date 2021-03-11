78f3cb747fa4bdb9784309cb7479bda569e1b7b3
fellype <fellype.ads@gmail.com>
2021-03-10 21:12:17 -0300

    verify-token-direct-to-login

--- src/autenntication/PrivateRoute.js
+++ src/autenntication/PrivateRoute.js
@@ -1,26 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Route, Redirect } from "react-router-dom";
 
 import { AuthContext } from "./authContext";
+import api from "./api";
 
 function PrivateRoute({ component: Component, ...rest }) {
   const authContext = useContext(AuthContext);
+
+  const [validToken, setValidToken] = useState(0);
+  useEffect(async () => {
+    try {
+      const response = await api.get(
+        `${process.env.REACT_APP_API_BASE}usuario/verifytoken`
+      );
+    } catch (error) {
+      if (error.response.status === 401) {
+        setValidToken(401);
+      }
+      console.error(error);
+    }
+  }, []);
   return (
     <Route
       {...rest}
       render={(routeProps) => {
         if (authContext.loggedInUser) {
           if (authContext.loggedInUser.user._id) {
-            return <Component {...routeProps} {...rest} />;
-          } else {
-            return (
-              <Redirect
-                to={{
-                  pathname: "/",
-                  state: { from: routeProps.location },
-                }}
-              />
-            );
+            if (validToken === 0) {
+              return <Component {...routeProps} {...rest} />;
+            } else {
+              return (
+                <Redirect
+                  to={{
+                    pathname: "/",
+                    state: { from: routeProps.location },
+                  }}
+                />
+              );
+            }
           }
         } else {
           return (
