ed033f295eb1f7615035390944dc91e0979a1dc1
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-03-16 00:54:32 -0300

    Ajustes

--- config/scripts/after_install.sh
+++ config/scripts/after_install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/app
 echo "CD Realizado"
-npm install
-npm install --save react react-dom react-scripts react-particles-js
-npm install pm2 -g
-echo "Instalações concluídas com sucesso"
 sudo mv /home/ubuntu/app/build /var/www/observador/ 
 echo "Atualizando Servidor com Novos Arquivos"
 sudo rm -r /home/ubuntu/app/
--- config/scripts/before_install.sh
+++ config/scripts/before_install.sh
@@ -3,5 +3,5 @@ cd /home/ubuntu/app
 echo "Cd realizado com Sucesso"
 sudo cp -R /var/www/observador /home/ubuntu/app/$(date +'%Y-%m-%d-%T')
 echo "BackUp do Fonte Anterior Salvo em: /home/ubuntu/api/"$(date +'%Y-%m-%d-%T')
-sudo rm -rf /var/www/observador/ 
-echo "Limpando Pasta do Servidor"w
+sudo rm -r /var/www/observador/ 
+echo "Limpando Pasta do Servidor"
