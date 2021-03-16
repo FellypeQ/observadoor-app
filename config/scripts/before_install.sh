08da8348697fe015b4fe68372242ee6f39921b15
wellingtonolive <wellingtoncarvalho11@gmail.com>
2021-03-15 21:00:49 -0300

    Pipeline

---
+++ config/scripts/before_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/api
+echo "Cd realizado com Sucesso"
+sudo cp -R /opt/app/observador /home/ubuntu/api/$(date +'%Y-%m-%d-%T')
+echo "BackUp do Fonte Anterior Salvo em: /home/ubuntu/api/"$(date +'%Y-%m-%d-%T')
