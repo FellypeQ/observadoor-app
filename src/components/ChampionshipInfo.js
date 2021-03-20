110f2b4caafa404bc9d432e5fb7303fd91f35520
fellype <fellype.ads@gmail.com>
2021-03-20 18:20:08 -0300

    loading, login, lista de campeonatos, e detalhes campeonato

--- src/components/ChampionshipInfo.js
+++ src/components/ChampionshipInfo.js
@@ -1,64 +1,96 @@
-import { React } from "react";
+import { React, useState } from "react";
+import { LinearProgress, CircularProgress } from "@material-ui/core";
 
 function ChampionshipInfo(props) {
+  function defineProgress(stage) {
+    if (stage === 1) {
+      return 33;
+    }
+    if (stage === 2) {
+      return 66;
+    } else {
+      return 99;
+    }
+  }
+
   return (
     <div>
-      <input
-        type="text"
-        className="text-30px wid-90 disp-flex just-center mg-b-2"
-        placeholder="Nome do Campeonato "
-        name="name"
-        value={props.name}
-        onChange={props.handleChange}
-        disabled={props.disabled}
-      />
+      {props.loading ? (
+        <CircularProgress />
+      ) : (
+        <input
+          type="text"
+          className="text-30px wid-90 disp-flex just-center mg-b-2"
+          placeholder="Nome do Campeonato "
+          name="name"
+          value={props.name}
+          onChange={props.handleChange}
+          disabled={props.disabled}
+        />
+      )}
       <div className="disp-flex just-end mg-b-2">
         <h4>{props.nomeObsevador}</h4>
       </div>
       <div className="disp-flex flex-direct-col align-start">
         <label className="wid-100 mg-b-2">
           Local:
-          <input
-            type="text"
-            placeholder="Local do jogo"
-            name="localization"
-            value={props.localization}
-            onChange={props.handleChange}
-            disabled={props.disabled}
-          />
+          {props.loading ? (
+            <CircularProgress size={15} />
+          ) : (
+            <input
+              type="text"
+              placeholder="Local do jogo"
+              name="localization"
+              value={props.localization}
+              onChange={props.handleChange}
+              disabled={props.disabled}
+            />
+          )}
         </label>
         <label className="wid-100 mg-b-2">
           Início da competição:
-          <input
-            type="date"
-            name="competionDate"
-            value={props.competionDate}
-            onChange={props.handleChange}
-            disabled={props.disabled}
-          />
+          {props.loading ? (
+            <CircularProgress size={15} />
+          ) : (
+            <input
+              type="date"
+              name="competionDate"
+              value={props.competionDate}
+              onChange={props.handleChange}
+              disabled={props.disabled}
+            />
+          )}
         </label>
         <label className="wid-100 mg-b-2">
           Categorias:
-          <input
-            type="text"
-            placeholder="Categorias"
-            name="category"
-            value={props.category}
-            onChange={props.handleChange}
-            disabled={props.disabled}
-          />
+          {props.loading ? (
+            <CircularProgress size={15} />
+          ) : (
+            <input
+              type="text"
+              placeholder="Categorias"
+              name="category"
+              value={props.category}
+              onChange={props.handleChange}
+              disabled={props.disabled}
+            />
+          )}
         </label>
         <label className="wid-100 mg-b-2">
           Responsável:
-          <input
-            className="wid-60"
-            type="text"
-            placeholder="Responsável pelo campeonato"
-            name="responsable"
-            value={props.responsable}
-            onChange={props.handleChange}
-            disabled={props.disabled}
-          />
+          {props.loading ? (
+            <CircularProgress size={15} />
+          ) : (
+            <input
+              className="wid-60"
+              type="text"
+              placeholder="Responsável pelo campeonato"
+              name="responsable"
+              value={props.responsable}
+              onChange={props.handleChange}
+              disabled={props.disabled}
+            />
+          )}
         </label>
       </div>
     </div>
--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -2,10 +2,22 @@ import { React, useContext, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { AuthContext } from "../../autenntication/authContext";
 import api from "../../autenntication/api";
+import {
+  CircularProgress,
+  InputLabel,
+  InputBases,
+  InputBase,
+} from "@material-ui/core";
 
 function Login(props) {
   const [formLogin, setFormLogin] = useState({ email: "", senha: "" });
   const authContext = useContext(AuthContext);
+  const [loading, setLoading] = useState({
+    login: false,
+  });
+  const [error, setError] = useState({
+    login: "",
+  });
 
   const history = useHistory();
 
@@ -19,20 +31,33 @@ function Login(props) {
     event.preventDefault();
     localStorage.clear();
 
+    setLoading({
+      ...loading,
+      login: true,
+    });
+
     try {
-      const response = await api.post(
+      const respLogin = await api.post(
         `${process.env.REACT_APP_API_BASE}usuario/login`,
         formLogin
       );
-      authContext.setLoggedInUser({ ...response.data });
+      authContext.setLoggedInUser({ ...respLogin.data });
       localStorage.setItem(
         "loggedInUser",
-        JSON.stringify({ ...response.data })
+        JSON.stringify({ ...respLogin.data })
       );
       history.push("/campeonatos");
       window.location.reload(true);
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      setLoading({
+        ...loading,
+        login: false,
+      });
+      setError({
+        ...error,
+        login: error.response.status,
+      });
+      console.error("respLogin", error);
     }
   };
 
@@ -48,29 +73,45 @@ function Login(props) {
         <label className="text-24px text-center">
           Bem vindo ao Observadoor!
         </label>
-        <label>E-mail</label>
-        <input
-          type="email"
-          placeholder="digite seu e-mail"
-          name="email"
-          value={formLogin.email}
-          onChange={handleChange}
-        />
-        <label>Senha</label>
-        <input
-          type="password"
-          placeholder="Digite sua senha"
-          name="senha"
-          value={formLogin.senha}
-          onChange={handleChange}
-        />
-        <button className="btn btn-black text-18px">Entrar </button>
-        <Link
-          to={`/register`}
-          className="btn btn-blue text-14px mg-b-5 text-decore-none"
-        >
-          Não tem Cadastro? Cadastre-se!
-        </Link>
+        {loading.login ? (
+          <CircularProgress color="primary" size={80} />
+        ) : (
+          <>
+            <label>E-mail</label>
+            <input
+              className={`${error.login === 401 && "error-border"}`}
+              type="email"
+              placeholder="digite seu e-mail"
+              name="email"
+              value={formLogin.email}
+              onChange={handleChange}
+            />
+            <label>Senha</label>
+            <input
+              className={`${error.login === 401 && "error-border"}`}
+              type="password"
+              placeholder="Digite sua senha"
+              name="senha"
+              value={formLogin.senha}
+              onChange={handleChange}
+            />
+            {error.login === 401 && (
+              <p className="error">Email ou senha incorretos</p>
+            )}
+            {error.login === 500 && (
+              <p className="error">
+                Erro de conexão, por favor tente novamente
+              </p>
+            )}
+            <button className="btn btn-black text-18px">Entrar </button>
+            <Link
+              to={`/register`}
+              className="btn btn-blue text-14px mg-b-5 text-decore-none"
+            >
+              Não tem Cadastro? Cadastre-se!
+            </Link>
+          </>
+        )}
       </form>
     </div>
   );
